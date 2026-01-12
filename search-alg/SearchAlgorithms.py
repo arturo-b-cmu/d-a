09af8e4ab128dfa26e8dc6416ce4220504118cc4
Arturo Bohuchot <arturo@Arturos-MacBook-Pro-2.local>
2026-01-12 16:17:03 -0500

    fix issue

---
+++ search-alg/SearchAlgorithms.py
@@ -0,0 +1,21 @@
+#linear search algorithms does not matter the order of the elements
+    #O(n) (worst case) Time complexity dependent on the size of lst or 'n'
+def linearSearch(lst, target):
+    for i in range(len(lst)):
+        if lst[i] == target:
+            return True
+    return False
+
+
+print(linearSearch((1,6,2,7), 4))
+
+
+def recursiveLS(lst, target): #worst case O(n)^2
+    if lst == []: #O(1)
+        return False
+    elif lst[0] == target: #O(1)
+        return True
+    else:
+        smallerProblem = lst[1:] #list slicing is an operation that takes O(n)
+        smallerResult = recursiveLS(smallerProblem, target)
+        return smallerResult #change
